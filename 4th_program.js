84e694ff84be7d0abfb1951b9e39a5fd36bdb7b8
Your Name <you@example.com>
2020-07-07 23:22:04 +0600

    this is javaScript program

--- 4th_program.js
+++ 4th_program.js
@@ -1,7 +1,61 @@
-// const student = [1, 4, 45, 65, 56, 23];
-// console.log(student[9]);
-// console.log(student);
+// map used.....
 
-let name = ["habib", "wahid", "tarek", "siyam", "rifat"];
-console.log(name[10]);
-console.log(name);
+// dobuleIt to array...
+const numbers = [2, 4, 5, 6, 7, 8];
+const output = [];
+
+for(let i = 0; i < numbers.length; i++){
+    const element = numbers[i];
+    const result = element * element;
+    output.push(result);
+}
+console.log(output);
+
+
+// system of methods 2nd... system of methods 2nd...
+const numbers = [2, 4, 5, 6, 7, 8];
+ 
+function square(element){
+    const result = element * element;
+    return result;
+}
+const total = numbers.map(square)
+console.log(total);
+
+
+// system of methods 3rd... system of methods 3rd...
+const numbers = [2, 4, 5, 6, 7, 8];
+
+const total = numbers.map(function square(element){
+        const result = element * element;
+        return result;
+})
+console.log(total);
+
+
+//system of methods 3rd... system of methods 3rd...
+const numbers = [2, 4, 5, 6, 7, 8];
+
+const result = numbers.map(function square(element){
+    return element * element;
+})
+console.log(result);
+
+
+// system of methods 4th... system of methods 4th...
+const numbers = [2, 4, 5, 6, 7, 8];
+
+numbers.map(function square(element, index, array){
+    console.log(element, index, array);
+})
+
+
+// system of methods 5th..... system of methods 5th.....
+const numbers = [2, 4, 5, 6, 7, 8];
+const square = numbers.map(element => element * element);
+console.log(square);
+
+// system of methods 6th..... system of methods 6th.....
+const numbers = [2, 4, 5, 6, 7, 8];
+const y = numbers.map(x => x * x);
+console.log(y);
