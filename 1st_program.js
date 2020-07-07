f159d5e874472bb856d595ccfd6d86798ce5ab94
Your Name <you@example.com>
2020-07-07 08:56:10 +0600

    hi jonathon

--- 1st_program.js
+++ 1st_program.js
@@ -1,7 +1,8 @@
 /** false value in JavaScrit program:
 1. let number = 0;
-2. let name = ''; (empty string is false!)
-3. let name = ""; (empty string is false!)
+2. let name = '';  (empty string is false!)
+3. let name = "";  (empty string is false!)
+4. let name = ``;  (empty string is false!)
 4. undefiend variable.  (example: let name;), (let name = undefiend)
 5. variable defienf to null.  (example: const number = null;)
 6.variable defienf to NaN.  (example: const number = NaN;)
@@ -15,11 +16,12 @@
  4. let number = {};
  5. let name = " ";  (white space string is true!)
  6. let name = ' ';  (white space string is true!)
- 7. let condition = false;
+ 7. let name = ` `;  (white space string is true!)
+ 8. let condition = false;
  */
 
 // this is base ....
-let age = NaN;
+let age = 10;
 
 if(age){
     console.log("condition is true!");
