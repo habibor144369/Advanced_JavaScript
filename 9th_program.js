72e22335b8a4a982350df20cef4916ea5da1fe6a
Your Name <you@example.com>
2020-07-11 16:51:59 +0600

    hello javascript

--- 9th_program.js
+++ 9th_program.js
@@ -1,15 +1,15 @@
 // Array slice, splice, array join elements......
 // array slice in array...
-// const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-// const part = numbers.slice(2, 5);  // 2 index, 5 index..
-// console.log(part);
-// console.log(numbers);  // The case of slice, orginal array stay same no change.
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+const part = numbers.slice(2, 5);  // 2 index, 5 index..
+console.log(part);
+console.log(numbers);  // The case of slice, orginal array stay same no change.
 
-// // array splice in array.....
-// const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-// const removed = numbers.splice(2, 5, 20, 25, 30)  // 2 index, 5 digit count of number in remooved array, and 20,25,30 new elements add in numbers array.
-// console.log(removed);
-// console.log(numbers);  // The case of splice, orginal array keep to change...
+// array splice in array.....
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+const removed = numbers.splice(2, 5, 20, 25, 30)  // 2 index, 5 digit count of number in remooved array, and 20,25,30 new elements add in numbers array.
+console.log(removed);
+console.log(numbers);  // The case of splice, orginal array keep to change...
 
 // array join methods....
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
